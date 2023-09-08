38fd807997267e375155e626fae32095fe4616ba
Aditya Bhattad <adityabhattad18@gmail.com>
2023-09-08 18:36:48 +0530

    setup: add ingress config

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -3,8 +3,11 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
-
+app.get('/api/users/currentUser',(req,res)=>{
+  res.status(200).send({Hello:"World"})
+})
 
 app.listen(3000, () => {
   console.log("Listening on port 3000!");
 });
+
