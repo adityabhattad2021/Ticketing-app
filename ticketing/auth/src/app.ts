391cebf18488d69e478163cbd7adf683e9c91562
Aditya Bhattad <adityabhattad18@gmail.com>
2023-11-29 17:23:51 +0530

    feat: fetch current user status in server component

--- ticketing/auth/src/app.ts
+++ ticketing/auth/src/app.ts
@@ -13,7 +13,6 @@ app.use(
   cookieSession({
     signed:false,
     secure:process.env.NODE_ENV!=='test',
-
   })
 )
 
--- ticketing/client/app/page.tsx
+++ ticketing/client/app/page.tsx
@@ -1,9 +1,33 @@
+import axios from "axios"
 import Image from 'next/image'
+import Link from "next/link"
 
-export default function Home() {
+async function getAuthStatus() {
+  let response;
+  if(typeof window==='undefined'){
+    response = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentUser',
+      {
+        headers: {
+          Host: "ticketing.xyz"
+        }
+      }
+    );
+  }else{
+    response= await axios.get('/api/users/currentUser');
+  }
+  return response.data;
+}
+
+export default async function Home() {
+  const currentUser = await getAuthStatus();
+  console.log(currentUser);
+  
   return (
     <div>
-      Hello, World asdasd
+      <Link href={"/auth/sign-up"}>
+        SignUp
+      </Link>
     </div>
   )
 }
