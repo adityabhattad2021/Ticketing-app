73d23b5c03715a2e2a0e20f71b30976daea170cf
Aditya Bhattad <adityabhattad18@gmail.com>
2023-11-29 17:42:51 +0530

    fix: directly pass the header from request to ingress-ngnix

--- ticketing/client/app/page.tsx
+++ ticketing/client/app/page.tsx
@@ -1,20 +1,26 @@
 import axios from "axios"
-import Image from 'next/image'
 import Link from "next/link"
+import { headers } from 'next/headers'
+
 
 async function getAuthStatus() {
+
+
   let response;
-  if(typeof window==='undefined'){
+  if (typeof window === 'undefined') {
+    const headersObj = {};
+    headers().forEach((header, key) => {
+      //@ts-ignore
+      headersObj[key] = header;
+    });
     response = await axios.get(
       'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentUser',
       {
-        headers: {
-          Host: "ticketing.xyz"
-        }
+        headers: headersObj
       }
     );
-  }else{
-    response= await axios.get('/api/users/currentUser');
+  } else {
+    response = await axios.get('/api/users/currentUser');
   }
   return response.data;
 }
@@ -22,7 +28,7 @@ async function getAuthStatus() {
 export default async function Home() {
   const currentUser = await getAuthStatus();
   console.log(currentUser);
-  
+
   return (
     <div>
       <Link href={"/auth/sign-up"}>
