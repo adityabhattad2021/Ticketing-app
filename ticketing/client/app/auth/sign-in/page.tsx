22bb455a100172b94541c7b3d9aa228ddb44f0ca
Aditya Bhattad <adityabhattad18@gmail.com>
2023-11-30 17:44:45 +0530

    feat: add signin form

---
+++ ticketing/client/app/auth/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import SignInForm from "@/components/SignIn";
+
+export default function SignInPage() {
+    return (
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[80%] sm:w-[30%] flex justify-center ">
+            <SignInForm />
+        </div>
+    )
+}
--- ticketing/client/components/SignIn.tsx
+++ ticketing/client/components/SignIn.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
-import { useForm } from "react-hook-form"
+import { useForm } from "react-hook-form";
 import { useToast } from "./ui/use-toast";
 import {
     Card,
@@ -25,19 +25,17 @@ import { useRouter } from "next/navigation";
 import axios from "axios";
 
 
-
 const formSchema = z.object({
     email: z.string(),
     password: z.string().min(8, {
         message: "Minimum length for password should be 8."
     }).max(20, {
-        message: 'Password max be 20 characters big.'
+        message: "Password max can be 20 characters big."
     })
 })
 
 
-export default function SignUpFrom() {
-
+export default function SignInForm() {
     const router = useRouter();
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
@@ -46,21 +44,18 @@ export default function SignUpFrom() {
             password: ""
         }
     })
+
     const { toast } = useToast();
 
-    const isLoading = form.formState.isSubmitted;
-
     async function handleOnSubmit(values: z.infer<typeof formSchema>) {
-        console.log(values);
-
         try {
             await axios.post(
-                '/api/users/signup',
+                '/api/users/signin',
                 values
             )
             toast({
                 title: "Welcome",
-                description: "Successfully signed up!"
+                description: "Successfully signed in!"
             })
             router.push('/');
         } catch (error) {
@@ -74,15 +69,14 @@ export default function SignUpFrom() {
         }
     }
 
-
     return (
-        <Card>
+        <Card className="w-full">
             <CardHeader>
                 <CardTitle>
-                    Signup
+                    Sign In
                 </CardTitle>
                 <CardDescription>
-                    Sign up to this Ticketing App (alpha preview).
+                    Sign in to the application
                 </CardDescription>
             </CardHeader>
             <CardContent>
@@ -97,14 +91,10 @@ export default function SignUpFrom() {
                                         <FormLabel>Email</FormLabel>
                                         <FormControl>
                                             <Input
-                                                placeholder="name@email.com"
+                                                placeholder="johndoe@email.com"
                                                 {...field}
                                             />
                                         </FormControl>
-                                        <FormDescription>
-                                            This is your identifier throughout the app.
-                                        </FormDescription>
-                                        <FormMessage />
                                     </FormItem>
                                 )
                             }}
@@ -115,27 +105,29 @@ export default function SignUpFrom() {
                             render={({ field }) => {
                                 return (
                                     <FormItem>
-                                        <FormLabel>Password</FormLabel>
+                                        <FormLabel>
+                                            Password
+                                        </FormLabel>
                                         <FormControl>
                                             <Input
                                                 placeholder="your password here..."
                                                 {...field}
                                             />
                                         </FormControl>
-                                        <FormDescription>
-                                            We suggest you to take a note of your password, as we do have forgot password functionality just yet.
-                                        </FormDescription>
-                                        <FormMessage />
                                     </FormItem>
                                 )
                             }}
                         />
-                        <div className="w-full flex justify-center items-center" >
-                            <Button size={"lg"} type="submit">Submit</Button>
+                        <div className="w-full flex justify-center items-center">
+                            <Button size={"lg"} type="submit">
+                                Submit
+                            </Button>
                         </div>
                     </form>
                 </Form>
             </CardContent>
         </Card>
     )
+
 }
+
