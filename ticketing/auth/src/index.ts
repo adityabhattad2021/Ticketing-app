b307479eeb22667a07562af0fc5344bfce06d7c2
Aditya Bhattad <adityabhattad18@gmail.com>
2023-12-05 16:09:06 +0530

    fix: tickets infra config

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -6,8 +6,11 @@ const start = async () => {
   if(!process.env.JWT_KEY){
     throw new Error('JWT_KEY_MUST_BE_DEFINED');
   }
+  if(!process.env.MONGO_URI){
+    throw new Error('MONGO_URI_MUST_BE_DEFINED');
+  }
   try{
-    await mongoose.connect('mongodb://auth-mongo-service:27017/auth');
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('[SUCCESSFULLY_CONNECTED_TO_MONGODB]');
   }catch(error){
     console.log('[ERROR_CONNECTING_TO_MONGODB]: ',error);
