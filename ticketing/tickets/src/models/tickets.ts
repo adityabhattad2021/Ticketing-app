8058dc7faf50fbfee19bb3b8086c4a732edcb12d
Aditya Bhattad <adityabhattad18@gmail.com>
2023-12-07 14:24:35 +0530

    feat: add ticket model

--- ticketing/tickets/src/models/tickets.ts
+++ ticketing/tickets/src/models/tickets.ts
@@ -18,3 +18,34 @@ interface TicketDoc extends mongoose.Document {
 interface TicketModel extends mongoose.Model<TicketDoc>{
     build(attr:TicketAttrs):TicketDoc
 }
+
+
+const ticketSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    price:{
+        type: Number,
+        required:true
+    },
+    userId:{
+        type:String,
+        required:true
+    }
+},{
+    toJSON:{
+        transform(doc,ret){
+            ret.id = ret._id;
+            delete ret._id;
+        }
+    }
+});
+
+
+ticketSchema.statics.build = (attr:TicketAttrs)=>{
+    return new Ticket(attr);
+}
+
+const Ticket = mongoose.model<TicketDoc,TicketModel>('Ticket',ticketSchema);
+
