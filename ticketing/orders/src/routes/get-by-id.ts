4101179caa6f08457003f4b5394784d789bf0a86
Aditya Bhattad <adityabhattad18@gmail.com>
2023-12-19 13:16:24 +0530

    feat: add get order by id route

---
+++ ticketing/orders/src/routes/get-by-id.ts
@@ -0,0 +1,25 @@
+import { NotFoundError, RequireAuth,NotAuthorizedError } from "@gittix-microservices/common";
+import express,{Request,Response} from "express"
+import { Order } from "../models/order";
+
+const router = express.Router();
+
+router.get(
+    '/api/orders/:orderId',
+    RequireAuth,
+    async (req:Request,res:Response)=>{
+        const orderId = req.params.orderId;
+
+        const order = await Order.findById(orderId).populate('ticket');
+        if(!order){
+            throw new NotFoundError();
+        }
+        if(order.userId!==req.currentUser!.id){
+            throw new NotAuthorizedError();
+        }
+
+        res.status(200).send(order);
+    }
+)
+
+export {router as getOrderByIdRouter};
