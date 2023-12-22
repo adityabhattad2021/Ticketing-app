b57232fe00f5860aac2f48e0d6cb39102df3923d
Aditya Bhattad <adityabhattad18@gmail.com>
2023-12-22 18:35:46 +0530

    test: add test for ticket concurrency control

--- ticketing/tickets/src/models/__test__/ticket.test.ts
+++ ticketing/tickets/src/models/__test__/ticket.test.ts
@@ -1,1 +1,25 @@
-it.todo("Implements optimistic concurrency control");
+import { Ticket } from "../tickets";
+
+it("Implements optimistic concurrency control",async ()=>{
+    const ticket = Ticket.build({
+        title:'A new ticket',
+        price:200,
+        userId:'abc'
+    });
+    await ticket.save();
+
+    const firstInstance = await Ticket.findById(ticket.id);
+    const secondInstance = await Ticket.findById(ticket.id);
+
+    firstInstance!.set({price:10});
+    secondInstance!.set({price:15});
+
+    await firstInstance!.save();
+
+    try{
+        await secondInstance!.save();
+    }catch (_){
+        return;
+    }
+    throw new Error("Should not reach to this point");
+});
