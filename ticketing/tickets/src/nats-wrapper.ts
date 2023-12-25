6c5bcc77f703b01a0595e2213a898720c8992a1a
Aditya Bhattad <adityabhattad18@gmail.com>
2023-12-25 23:03:30 +0530

    chore: move nats-wrapper implementation to the common module

--- ticketing/tickets/src/nats-wrapper.ts
+++ ticketing/tickets/src/nats-wrapper.ts
@@ -1,29 +1,3 @@
-import nats,{ Stan } from "node-nats-streaming";
-
-class NatsWrapper {
-    private _client?:Stan;
-
-    get client(){
-        if(!this._client){
-            throw new Error('Cannot access NATS client');
-        }
-        return this._client;
-    }
-
-    connect(clusterId:string,clientId:string,url:string){
-        this._client = nats.connect(clusterId,clientId,{url});
-
-        return new Promise<void>((resolve,reject)=>{
-            this.client.on('connect',()=>{
-                console.log('Successfully connected to the NATS client!');
-                resolve();
-            })
-            this.client.on('error',(err)=>{
-                console.log('There was an error while connecting to the NATS client');
-                reject(err)
-            })
-        })
-    }
-}
+import { NatsWrapper } from "@gittix-microservices/common";
 
 export const natsWrapper = new NatsWrapper();
