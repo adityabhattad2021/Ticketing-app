298beb3a011e16574a7a30fefa92ca3ba19aa8fd
Aditya Bhattad <adityabhattad18@gmail.com>
2023-12-25 16:54:58 +0530

    feat: add order updated event

--- ticketing/tickets/src/events/listener/order-cancelled-listener.ts
+++ ticketing/tickets/src/events/listener/order-cancelled-listener.ts
@@ -1,13 +1,34 @@
-import { Listener,OrderCancelledEvent, Subjects } from "@gittix-microservices/common";
+import { Listener, OrderCancelledEvent, Subjects } from "@gittix-microservices/common";
 import { queueGroupName } from "./constants/queue-group-name";
 import { Message } from "node-nats-streaming";
+import { Ticket } from "../../models/tickets";
+import { TicketUpdatedPublisher } from "../publisher/ticket-updated-publisher";
 
 
 export class OrderCancelledListener extends Listener<OrderCancelledEvent>{
     readonly subject = Subjects.OrderCancelled;
     readonly queueGroupName: string = queueGroupName;
 
-    async onMessage(data:OrderCancelledEvent['data'],msg:Message){
-        
+    async onMessage(data: OrderCancelledEvent['data'], msg: Message) {
+        const ticket = await Ticket.findById(data.ticket.id);
+
+        if (!ticket) {
+            throw new Error("No ticket found for the provided id");
+        }
+
+        ticket.set({ orderId: undefined });
+
+        await ticket.save();
+
+        await new TicketUpdatedPublisher(this.client).publish({
+            id: ticket.id,
+            title: ticket.title,
+            price: ticket.price,
+            userId: ticket.userId,
+            version: ticket.version,
+            orderId: ticket.orderId
+        });
+
+        msg.ack();
     }
 }
