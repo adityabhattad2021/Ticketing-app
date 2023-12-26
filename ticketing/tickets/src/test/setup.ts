7a06b752705129e7affef17de46fcd215dc6faea
Aditya Bhattad <adityabhattad18@gmail.com>
2023-12-26 23:03:38 +0530

    chore: add stripe env to payment deployment

--- ticketing/tickets/src/test/setup.ts
+++ ticketing/tickets/src/test/setup.ts
@@ -1,58 +1,58 @@
-import {MongoMemoryServer} from "mongodb-memory-server";
+import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
 
-declare global{
-    var signIn:()=>string[];
+declare global {
+    var signIn: () => string[];
 }
 
 jest.mock('../nats-wrapper');
 
-let mongo:any;
-beforeAll(async ()=>{
-    process.env.JWT_KEY='abcd';
+let mongo: any;
+beforeAll(async () => {
+    process.env.JWT_KEY = 'abcd';
     mongo = await MongoMemoryServer.create({
-        binary:{
-            version:'6.0.4'
+        binary: {
+            version: '6.0.4'
         }
     })
     const mongoUri = mongo.getUri();
 
-    await mongoose.connect(mongoUri,{});
+    await mongoose.connect(mongoUri, {});
 })
 
 
-beforeEach(async ()=>{
+beforeEach(async () => {
 
     // clear all the fake function implementation data
     jest.clearAllMocks();
 
     const collections = await mongoose.connection.db.collections();
 
-    for(let collection of collections){
+    for (let collection of collections) {
         await collection.deleteMany({})
     }
 })
 
 
-afterAll(async ()=>{
-    if(mongo){
+afterAll(async () => {
+    if (mongo) {
         await mongo.stop();
     }
     await mongoose.connection.close();
 })
 
 
-global.signIn = () =>{
+global.signIn = () => {
     const randomId = new mongoose.Types.ObjectId().toHexString()
     const payload = {
         // id:'1234567890',
-        id:randomId,
-        email:'test@test.com'
+        id: randomId,
+        email: 'test@test.com'
     }
-    const token = jwt.sign(payload,process.env.JWT_KEY!);
+    const token = jwt.sign(payload, process.env.JWT_KEY!);
 
-    const session = {jwt:token};
+    const session = { jwt: token };
 
     const sessionJSON = JSON.stringify(session);
 
