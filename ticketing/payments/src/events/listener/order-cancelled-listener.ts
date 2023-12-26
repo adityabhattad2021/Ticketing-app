2fb73d75afbf51f820bf0e5135687fb5041306c7
Aditya Bhattad <adityabhattad18@gmail.com>
2023-12-26 19:31:26 +0530

    setup: add order cancelled listener

---
+++ ticketing/payments/src/events/listener/order-cancelled-listener.ts
@@ -0,0 +1,22 @@
+import { Listener, OrderCancelledEvent, OrderStatus, Subjects } from "@gittix-microservices/common";
+import { queueGroupName } from "./constants/queue-group-name";
+import { Message } from "node-nats-streaming";
+import { Order } from "../../models/order";
+
+export class OrderCancelledListener extends Listener<OrderCancelledEvent>{
+    readonly subject = Subjects.OrderCancelled;
+    readonly queueGroupName: string = queueGroupName;
+
+    async onMessage(data: OrderCancelledEvent['data'], msg: Message) {
+        const order = await Order.findById(data.id);
+        if (!order) {
+            throw new Error('No order found for the provided id');
+        }
+        order.set({
+            status: OrderStatus.Cancelled,
+        }),
+            await order.save();
+
+        msg.ack();
+    }
+}
