afb3a2f1be951968f855ff1a415ad99785b437cb
Aditya Bhattad <adityabhattad18@gmail.com>
2024-01-14 12:36:14 +0530

    perf: migrate to nextjs14

--- ticketing/client/next.config.js
+++ ticketing/client/next.config.js
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-// to be added later with nextjs does not responds properly
-// webpack: (config) => {
-//     config.watchOptions.poll = 300;
-//     return config;
-//   },
-
 module.exports = nextConfig
